8831e662ef112e8b85761da9ac02080e0f955aa5
suppynoob1 <andhuang@ucsc.edu>
2022-05-23 13:21:32 -0700

    added buttons 0-9, assigned onclick functions to them but did not yet write out what the functions do

--- myscript.js
+++ myscript.js
@@ -1,35 +1,61 @@
 let n1 = 4
 let n2 = 2
 
-const add = document.querySelector("#addButton")
-const minus= document.querySelector("#minusButton")
-const multiply = document.querySelector("#multiplyButton")
-const divide = document.querySelector("#divideButton")
-const operate = document.querySelector("#operateButton")
+const addButton = document.querySelector("#addButton")
+const minusButton = document.querySelector("#minusButton")
+const multiplyButton = document.querySelector("#multiplyButton")
+const divideButton = document.querySelector("#divideButton")
+const operateButton = document.querySelector("#operateButton")
+const oneButton = document.querySelector("#oneButton")
+const twoButton = document.querySelector("#twoButton")
+const threeButton = document.querySelector("#threeButton")
+const fourButton = document.querySelector("#fourButton")
+const fiveButton = document.querySelector("#fiveButton")
+const sixButton = document.querySelector("#sixButton")
+const sevenButton = document.querySelector("#sevenButton")
+const eightButton = document.querySelector("#eightButton")
+const nineButton = document.querySelector("#nineButton")
+const zeroButton = document.querySelector("#zeroButton")
 
-add.onclick = function() {
+
+addButton.onclick = add() 
+function add (){ 
     let sum = n1+n2
     console.log (sum)
 }
 
-minus.onclick = function() {
+minusButton.onclick = subtract()
+function subtract() {
     let difference = n1 - n2
     console.log(difference)
 }
 
-multiply.onclick = function() {
+multiplyButton.onclick = multiply()
+function multiply() {
     let product = n1 * n2 
     console.log(product)
 }
 
-divide.onclick = function() {
+divideButton.onclick = divide()
+function divide() {
     let quotient = n1/n2
     console.log (quotient) 
 }
 
-operate.addEventListener('click', () => {
+operateButton.addEventListener('click', () => {
     let arr = ["add", "minus", "multiply", "divide"]
     let random = arr[Math.floor(Math.random()*arr.length)]
     console.log(random)
     
 })
+
+oneButton.onclick = one()
+oneButton.onclick = two()
+oneButton.onclick = three()
+oneButton.onclick = four()
+oneButton.onclick = five()
+oneButton.onclick = six()
+oneButton.onclick = seven()
+oneButton.onclick = eight()
+oneButton.onclick = nine()
+oneButton.onclick = zero()
