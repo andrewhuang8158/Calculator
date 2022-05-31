28e7644bad3ccf55abf84d82ecd862c3bc38e41e
suppynoob1 <andhuang@ucsc.edu>
2022-05-31 16:23:39 -0700

    added keyboard input but broke button click functionality

--- myscript.js
+++ myscript.js
@@ -1,75 +1,133 @@
-let n1 = 4
-let n2 = 2
 
-let storedResult = '0';
+let firstNumber = ''
+let thisNumber = ''
+let shouldReset = false
+let currentOperation = null
+
+let active = document.querySelector("activeDisplay")
+let stored = document.querySelector("storedDisplay")
 
 const addButton = document.querySelector("#addButton")
 const minusButton = document.querySelector("#minusButton")
 const multiplyButton = document.querySelector("#multiplyButton")
 const divideButton = document.querySelector("#divideButton")
-const operateButton = document.querySelector("#operateButton")
-const oneButton = document.querySelector("#oneButton")
-const twoButton = document.querySelector("#twoButton")
-const threeButton = document.querySelector("#threeButton")
-const fourButton = document.querySelector("#fourButton")
-const fiveButton = document.querySelector("#fiveButton")
-const sixButton = document.querySelector("#sixButton")
-const sevenButton = document.querySelector("#sevenButton")
-const eightButton = document.querySelector("#eightButton")
-const nineButton = document.querySelector("#nineButton")
-const zeroButton = document.querySelector("#zeroButton")
 const dotButton = document.querySelector("#dotButton")
 const equalsButton = document.querySelector("#equalsButton")
 const clearButton = document.querySelector("#clearButton")
+const operateButton = document.querySelector("#operateButton")
+
+let numberButtons = document.querySelectorAll(".numberButton")
+let operatorButtons = document.querySelectorAll(".operatorButton")
+
+// const oneButton = document.querySelector("#oneButton")
+// const twoButton = document.querySelector("#twoButton")
+// const threeButton = document.querySelector("#threeButton")
+// const fourButton = document.querySelector("#fourButton")
+// const fiveButton = document.querySelector("#fiveButton")
+// const sixButton = document.querySelector("#sixButton")
+// const sevenButton = document.querySelector("#sevenButton")
+// const eightButton = document.querySelector("#eightButton")
+// const nineButton = document.querySelector("#nineButton")
+// const zeroButton = document.querySelector("#zeroButton")
+
+window.addEventListener("keydown", keyboardInput)
 
 
 
 
-addButton.onclick = add() 
-function add (){ 
-    let sum = n1+n2
-    console.log (sum)
-}
-
-minusButton.onclick = subtract()
-function subtract() {
-    let difference = n1 - n2
-    console.log(difference)
-}
-
-multiplyButton.onclick = multiply()
-function multiply() {
-    let product = n1 * n2 
-    console.log(product)
-}
-
-divideButton.onclick = divide()
-function divide() {
-    let quotient = n1/n2
-    console.log (quotient) 
-}
 
 operateButton.addEventListener('click', () => {
     let arr = ["add", "minus", "multiply", "divide"]
     let random = arr[Math.floor(Math.random()*arr.length)]
     console.log(random)
-    
 })
 
-oneButton.onclick = function one() {
-    storedResult += "1"
-    console.log(storedResult)
+numberButtons.onclick = function() {
+    addNum(numberButton.textContent)
 }
 
-twoButton.onclick = two()
-threeButton.onclick = three()
-fourButton.onclick = four()
-fiveButton.onclick = five()
-sixButton.onclick = six()
-sevenButton.onclick = seven()
-eightButton.onclick = eight()
-nineButton.onclick = nine()
-zeroButton.onclick = zero()
-dotButton.onclick = dot()
-equalsButton.onclick = equals()
-clearButton.onclick = clear()
+function addNum(number) {
+    if (activeDisplay.textContent == 0 || shouldReset) resetScreen()
+    activeDisplay.textContent += number
+    console.log(activeDisplay)
+}
+
+function resetScreen() {
+    activeDisplay.textContent = ''
+    shouldReset = false
+}
+
+
+operatorButtons.onclick = function() { 
+    addOperator(operatorButton.textContent)
+}
+
+function addOperator (operator) {
+    if (currentOperation !== null) equals()
+    firstNumber = activeDisplay.textContent
+    currentOperation = operator.textContent
+    storedDisplay.textContent = `${firstNumber} ${currentOperation}`
+    shouldReset = true
+}
+
+dotButton.onclick = function() {
+    addDot(dotButton.textContent)
+}
+
+function addDot() {
+    if (activeDisplay.textContent === '') activeDisplay = '0'
+    if (activeDisplay.textContent.includes('.'))return
+    activeDisplay += '.'
+}
+
+function equals() {
+    if (currentOperation == null || shouldReset) return
+    shouldReset == true
+    activeDisplay = `${firstNumber} ${thisNumber}`
+    firstNumber = activeDisplay.textContent
+    
+}
+
+function deleteNum() {
+    activeDisplay.textContent = activeDisplay.textContent.slice[0]
+}
+function clear() { 
+    if (activeDisplay !== '') resetScreen()
+    if (activeDisplay == '') {
+        storedDisplay = ''
+        firstNumber = ''
+        thisNumber = ''
+    }
+}
+// const clear = function() {
+//     activeDisplay = 0
+//     console.log(activeDisplay)
+// }
+
+function keyboardInput (e) { 
+    if (e.key >= 0 && e.key <= 9) addNum(e.key)
+    if (e.key == '.') addDot()
+    if (e.key == 'Backspace') deleteNum();
+    if (e.key == 'Enter' || e.key == '=') equals()
+    if (e.key == '+' || e.key == '*' || e.key == '/' || e.key == '-' ) addOperator(e.key)
+}
+
+function add(a,b) { 
+    return a + b 
+}
+function minus(a,b) { 
+    return a - b 
+}
+function multiply(a,b) { 
+    return a * b
+}
+function divide(a,b) { 
+    return a / b 
+}
+
+
+
+
+
+
+
